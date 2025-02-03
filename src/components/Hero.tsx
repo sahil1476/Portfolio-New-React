61d7b9e26b5ff28ccc62c7e59f37c6fedb8e1bc5
SAHIL SHARMA <94641122+sahil1476@users.noreply.github.com>
2025-02-03 19:57:53 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -55,33 +55,23 @@ export function Hero() {
       
       <div className="relative container mx-auto px-6 py-20 text-center text-white">
       
-      <div className="container mx-auto px-6 py-20 text-center drop-in-animation">
+{/*       <div className="container mx-auto px-6 py-20 text-center drop-in-animation">
         <h1 className="text-5xl md:text-6xl font-bold mb-6 glow-text">
           Sahil Sharma
         </h1>
         <p className="text-xl md:text-2xl mb-8 text-[#58a6ff] ">
           Aspiring Web Developer | Bachelor of Technology CSE
         </p>
-{/*         social icons */}
-{/*         <div className="flex justify-center md:justify-start space-x-6 mt-6 mb-10">
-                <a href="https://www.instagram.com/ssahilsharma_/" className="transform hover:scale-125 transition-transform duration-300 text-pink-500 hover:text-pink-600 dark:text-pink-400 dark:hover:text-pink-500">
-                  <Instagram className="w-9 h-9" />
-                </a>
-                <a href="https://www.facebook.com/profile.php?id=100012746700006" className="transform hover:scale-125 transition-transform duration-300 text-blue-600 hover:text-blue-700 dark:text-blue-500 dark:hover:text-blue-600">
-                  <Facebook className="w-9 h-9" />
-                </a>
-                <a href="https://www.linkedin.com/in/sahil-sharma-34989519a/" className="transform hover:scale-125 transition-transform duration-300 text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500">
-                  <Linkedin className="w-9 h-9" />
-                </a>
-                <a 
-                  href="mailto:krsahilsharma07@gmail.com" 
-                  className="transform hover:scale-125 transition-transform duration-300 text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-200"
-                >
-                  <Mail className="w-9 h-9" />
-                </a>
 
-              </div> */}
-      </div>  
+      </div>   */}
+        <div className="container mx-auto px-6 py-20 text-center">
+        <h1 className="image-text text-6xl md:text-9xl font-bold mb-6">
+          Sahil Sharma
+        </h1>
+        <p className="text-xl md:text-2xl mb-8 text-[#58a6ff]">
+          Aspiring Web Developer | Bachelor of Technology CSE
+        </p>
+      </div>
         <div className="flex justify-center space-x-6">
           <a 
             href="#projects" 
