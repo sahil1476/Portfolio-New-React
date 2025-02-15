4e9dc6e2b82ddcf2242f271f5dbe9b46f2995469
SAHIL SHARMA <94641122+sahil1476@users.noreply.github.com>
2025-02-15 11:44:34 +0530

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Briefcase } from 'lucide-react';
 
 const experiences = [
   {
+    title: 'Data Operation Intern',
+    logo: 'https://media.licdn.com/dms/image/v2/D4D0BAQFpdwevUGhzbQ/company-logo_200_200/company-logo_200_200/0/1733296869317/arnifiofficial_logo?e=2147483647&v=beta&t=LKSeNuABnsJa8eo9Y5mERsG49l9lYq67sQJF7lG3I3k',
+    company: 'Arnifi',
+    period: 'Jan 2025 - Present',
+    description: [
+      'Updating User Dashboard on Strapi',
+      'Compliance Task and Email',
+      'Notion Data management',
+      'Figma Flow Charts',
+      'Product Catalogue Management on Strapi',
+    ]
+  },
+  {
     title: 'Software Developer Intern',
+    logo: 'https://media.designrush.com/agencies/63605/conversions/XenonStack-logo-profile.jpg',
     company: 'XenonStack',
-    period: 'Jan 2024 - June 2024',
+    period: 'Jun 2024 - June 2024',
     description: [
       'Developed a responsive web application using React.js , TailwndCSS and GoLang ensuring seamless data flow between frontend and backend.',
       'Implemented data processing workflows using Apache Spark, enabling efficient handling of large-scale data.',
@@ -16,6 +29,7 @@ const experiences = [
   },
   {
     title: ' Data Analyst',
+    logo: 'https://media.licdn.com/dms/image/v2/D560BAQEnDna9cp5_jw/company-logo_100_100/company-logo_100_100/0/1722507705272/softadroit_logo?e=2147483647&v=beta&t=FfoxX26eg0K-rNKdF7x434jiTe0fQ0IFFGx_hoZ84fE',
     company: 'SoftAdroit System Solutions',
     period: 'July 2024 - Present',
     description: [
@@ -35,8 +49,12 @@ export function Experience() {
           {experiences.map((exp, index) => (
             <div key={index} className="mb-8 bg-[#161b22] rounded-lg p-6 transform hover:-translate-y-2 transition-all duration-300 hover:shadow-lg hover:shadow-[#58a6ff]/20">
               <div className="flex items-start">
-                <div className="p-2 bg-[#58a6ff]/10 rounded-lg mr-4">
-                  <Briefcase className="text-[#58a6ff]" size={24} />
+                <div className="w-12 h-12 rounded-lg overflow-hidden flex-shrink-0 mr-2 mt-2">
+                    <img
+                      src={exp.logo}
+                      alt={`${exp.company} logo`}
+                      className="w-full h-full object-cover"
+                    />
                 </div>
                 <div>
                   <h3 className="text-xl font-semibold text-white">{exp.title}</h3>
