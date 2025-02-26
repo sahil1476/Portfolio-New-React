6f592317d42fd41a4ed7e841d38da474131f23d3
SAHIL SHARMA <94641122+sahil1476@users.noreply.github.com>
2025-02-26 13:41:10 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <footer className="bg-[#0d1117] text-white py-12 relative">
       {/* Background GIFs */}
       <div
-        className="absolute bottom-0 left-0 w-48 h-48 opacity-90"
+        className="absolute bottom-0 left-0 w-48 h-48 opacity-90 mt-10"
         style={{
           backgroundImage: 'url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/22669da7-b18f-4b93-9b79-c22048bdd2b4/dhdvui5-04d7df85-9627-4a12-b062-15fa8f97a6ee.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzIyNjY5ZGE3LWIxOGYtNGI5My05Yjc5LWMyMjA0OGJkZDJiNFwvZGhkdnVpNS0wNGQ3ZGY4NS05NjI3LTRhMTItYjA2Mi0xNWZhOGY5N2E2ZWUuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.fRrAqaGwN1FqmGqidgAYK8EwYiga7ptPSZsWyvIXpk0)', // Replace with the actual GIF URL
           backgroundSize: 'cover',
@@ -15,7 +15,7 @@ export function Footer() {
         }}
       />
       <div
-        className="absolute bottom-0 right-0 w-48 h-48 opacity-90"
+        className="absolute bottom-0 right-0 w-48 h-48 opacity-90 mt-10"
         style={{
           backgroundImage: 'url(https://media2.giphy.com/media/5hvKNH3QAA4jPaMfcG/200w.gif?cid=6c09b952jcq34u1hp89wnk40ouexk1qizg7hxup5i66isi52&ep=v1_stickers_search&rid=200w.gif&ct=s)', // Replace with the actual GIF URL
           backgroundSize: 'cover',
